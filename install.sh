9676ffe17ca7876633313ce3cce7ee1b9bb516f3
ohp3x <ohp3x0@gmail.com>
2022-12-16 13:47:12 -0500

    added test install script

--- install.sh
+++ install.sh
@@ -2,15 +2,8 @@
 
 ## install suricata on ubuntu server 22.04.1 LTS
 
-sudo apt-get install build-essential libpcap-dev   \
-                libnet1-dev libyaml-0-2 libyaml-dev pkg-config zlib1g zlib1g-dev \
-                libcap-ng-dev libcap-ng0 make libmagic-dev         \
-                libgeoip-dev liblua5.1-dev libhiredis-dev libevent-dev \
-                python-yaml rustc cargo libpcre2-dev  \
-				libnetfilter-queue-dev libnetfilter-queue1  \
-                libnetfilter-log-dev libnetfilter-log1      \
-                libnfnetlink-dev libnfnetlink0  \
-                rustc cargo
-
+sudo apt-get install software-properties-common rustc cargo
+sudo add-apt-repository ppa:oisf/suricata-stable
+sudo apt-get update
+sudo apt-get install suricata
 sudo cargo install --force --debug --version 0.14.1 cbindgen
-
