9de756ceb296e5cae08074ad6c26841b83cf9165
ohp3x <ohp3x0@gmail.com>
2022-12-17 09:54:04 -0500

    switched to use OISF repo install in the docs

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/bash
 
-## install suricata on ubuntu server 22.04.1 LTS
+## Install Suricata on Ubuntu Server 22.04.1 LTS
+## [Reference:](https://suricata.readthedocs.io/en/latest/quickstart.html#installation)
 
 sudo apt-get install software-properties-common rustc cargo
 sudo add-apt-repository ppa:oisf/suricata-stable
 sudo apt-get update
-sudo apt-get install suricata
+sudo apt-get install suricata jq
 sudo cargo install --force --debug --version 0.14.1 cbindgen
+
+add /root/.caro/bin to path?
